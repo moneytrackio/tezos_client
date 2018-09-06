bbb5a25e7a8a480511b5c31090374356b4ed043a
pierre <pierre@moneytrack.io>
2018-09-06 10:15:54 +0200

    update log tests

--- spec/tezos_client/logger_spec.rb
+++ spec/tezos_client/logger_spec.rb
@@ -3,24 +3,23 @@ RSpec.describe TezosClient::Logger do
 
   subject { TezosClient.new }
 
-  class FileLogger
-    def initialize(path)
-      @target_file = path
-    end
-
-    def <<(obj)
-      File.open(@target_file, "a") { |f| f.puts obj }
-    end
-  end
-
-  let(:logfile) { Tempfile.new("log") }
-  let(:logger) do
-    FileLogger.new(logfile.path)
-  end
   describe "#logger=" do
-    it "writes in log file" do
-      TezosClient.logger = logger
-      expect { subject.log("hello") }.to change { logfile.rewind && logfile.read }.from("").to("hello\n")
+    context "pass logger class" do
+      let(:logger) { [] }
+      it "updates logger" do
+        TezosClient.logger = logger
+        expect { subject.log("hello") }.to change { logger }.from([]).to(["hello\n"])
+      end
+    end
+    context "pass file path" do
+      let(:logfile) { Tempfile.new("log") }
+
+      after { logfile.close && logfile.unlink }
+
+      it "updates log file" do
+        TezosClient.logger = logfile.path
+        expect { subject.log("hello") }.to change { logfile.rewind && logfile.read }.from("").to("hello\n")
+      end
     end
   end
 end
