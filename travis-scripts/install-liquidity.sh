668ba995beb37f1ce2d226a8e83565bfb988e033
pierre <pierre@moneytrack.io>
2018-09-07 16:04:14 +0200

    add liquidity to travis

---
+++ travis-scripts/install-liquidity.sh
@@ -0,0 +1,20 @@
+# This script is used in .travis.yml for continuous integration on travis.
+# BTW, it also show some needed system packages to build liquidity
+# Travis CI is done on Ubuntu trusty
+
+export OPAMYES=1
+wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
+yes "" | sh install.sh
+
+# currently, we only target OCaml 4.06.1 because we reuse the parser of OCaml
+opam switch create liquidity 4.06.1
+
+eval $(opam config env)
+
+opam update
+make build-deps
+make clone-tezos
+tezos/scripts/install_build_deps.raw.sh
+make -C tezos build-deps
+make
+sudo make install
