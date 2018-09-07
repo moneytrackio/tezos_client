d7388d47a12da521f9c3bc4b21fe1f8eb45f5c34
pierre <pierre@moneytrack.io>
2018-09-07 16:23:43 +0200

    disable some tests on travis

--- spec/tezos_client/rpc_interface/monitor_spec.rb
+++ spec/tezos_client/rpc_interface/monitor_spec.rb
@@ -1,30 +1,32 @@
 # frozen_string_literal: true
 
-RSpec.describe TezosClient::RpcInterface::Monitor do
-  subject { TezosClient::RpcInterface.new }
-  describe "#bootstrapped" do
-    around do |example|
-      disabling_vcr { example.call }
-    end
-
-    it "works" do
-      res = subject.bootstrapped
-
-      expect(res).to be_a Hash
-      expect(res).to have_key("block")
-      expect(res["block"]).to be_a String
-      expect(res).to have_key("timestamp")
-      expect(res["timestamp"]).to be_a DateTime
-    end
-
-    it "test monitor" do
-      received = []
-      monitoring_thread = subject.monitor("/monitor/heads/main") do |chunk|
-        received << chunk
+unless ENV["TRAVIS"]
+  RSpec.describe TezosClient::RpcInterface::Monitor do
+    subject { TezosClient::RpcInterface.new }
+    describe "#bootstrapped" do
+      around do |example|
+        disabling_vcr { example.call }
       end
-      sleep 60
-      monitoring_thread.kill
-      expect(received.size).to be > 1
+
+      it "works" do
+        res = subject.bootstrapped
+
+        expect(res).to be_a Hash
+        expect(res).to have_key("block")
+        expect(res["block"]).to be_a String
+        expect(res).to have_key("timestamp")
+        expect(res["timestamp"]).to be_a DateTime
+      end
+
+      it "test monitor" do
+        received = []
+        monitoring_thread = subject.monitor("/monitor/heads/main") do |chunk|
+          received << chunk
+        end
+        sleep 60
+        monitoring_thread.kill
+        expect(received.size).to be > 1
+      end
     end
   end
 end
