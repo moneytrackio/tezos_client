47580fcc903036c1421c9d3cbd4cb74c6d3c27de
Sebastien LAURET <sebastien@moneytrack.io>
2019-11-14 11:39:52 +0100

    feat(cmd): bach injection prevention
    convert the string cmd to array

--- lib/tezos_client/client_interface/block_contextual.rb
+++ lib/tezos_client/client_interface/block_contextual.rb
@@ -5,9 +5,9 @@ class TezosClient
     # Commands managing keys and accounts
     module BlockContextual
       def transfer(quantity:, from:, to:, dry_run: false, arg: nil)
-        cmd = "transfer #{quantity} from #{from} to #{to}"
-        cmd = "#{cmd} --dry-run" if dry_run
-        cmd = "#{cmd} --arg #{arg}" unless arg.nil?
+        cmd = ["transfer", quantity, "from", from, "to", to]
+        cmd << "--dry-run" if dry_run
+        cmd += ["--arg", "#{arg}"] unless arg.nil?
 
         res = call_client(cmd)
       end
--- lib/tezos_client/client_interface/key.rb
+++ lib/tezos_client/client_interface/key.rb
@@ -5,11 +5,11 @@ class TezosClient
     # Commands managing keys and accounts
     module Key
       def gen_keys(name)
-        call_client("gen keys #{name}")
+        call_client(["gen", "keys", name])
       end
 
       def addresses
-        output = call_client("list known addresses")
+        output = call_client(["list", "known", "addresses"])
         output.lines.reduce({}) do |acc, address_output|
           address_format = /([^:]+): (\w+) /
           res = address_format.match(address_output)
@@ -18,15 +18,15 @@ class TezosClient
       end
 
       def import_public_key(name, public_key, force: false)
-        cmd = "import public key #{name} #{public_key}"
-        cmd = "#{cmd} --force" if force
+        cmd = ["import", "public", "key", name, public_key]
+        cmd << "--force" if force
 
         call_client(cmd)
       end
 
       def import_secret_key(name, secret_key, force: false)
-        cmd = "import secret key #{name} #{secret_key}"
-        cmd = "#{cmd} --force" if force
+        cmd = ["import", "secret", "key", name, secret_key]
+        cmd << "--force" if force
 
         call_client(cmd)
       end
