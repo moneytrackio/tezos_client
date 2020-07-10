bae49d8b8311b92d424e94e24545cf23704a288c
EL KESRI Othmane <elkesri.othmane@gmail.com>
2020-07-10 15:20:30 +0200

    feat(tools): implem Tools::HashToMicheline and test it

---
+++ lib/tezos_client/tools/hash_to_micheline.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+class TezosClient::Tools::HashToMicheline < ActiveInteraction::Base
+  TYPES_MAPPING = {
+    int: :int,
+    nat: :int,
+    string: :string,
+    signature: :string,
+    bytes: :bytes,
+    timestamp: :int,
+    key: :string,
+    address: :string
+  }.freeze
+
+  string :contract_address, default: nil
+  string :entrypoint, default: nil
+  # example of params:
+  # {
+  #   spending_ref: "toto",
+  #   expires_at: Time.now
+  # }
+  hash :params, strip: false
+  hash :storage_type, strip: false, default: {}
+
+  # if storage_type is not received, it is fetched from the blockchain using
+  # contract_address and entrypoint (that are mandatory in this case)
+  validate :storage_type_or_contract_address_presence
+
+  def execute
+    return hash_type_to_hash_data(_storage_type.fetch(:prim), params.values.first) if params.size == 1
+
+    { prim: "Pair", args: generate_micheline(_storage_type[:args]) }
+  end
+
+  private
+    def generate_micheline(remaining_storage_type)
+      remaining_storage_type.each_with_object([]) do |h, acc|
+        next acc << { prim: "Pair", args: generate_micheline(h[:args]) } if h[:prim] == "pair"
+
+        annot = h[:annots].first.slice(1..-1).to_sym # remove '%'
+        acc << hash_type_to_hash_data(h[:prim], params.fetch(annot))
+      end
+    end
+
+    def convert_type(michelson_type)
+      TYPES_MAPPING.fetch(michelson_type.to_sym)
+    end
+
+    def hash_type_to_hash_data(michelson_type, value)
+      type = convert_type(michelson_type)
+
+      converted_value = case michelson_type.to_sym
+                        when :nat, :int
+                          value.to_s
+                        when :timestamp
+                          errors.add(:base, "timestamp input must be an instance of Time") unless value.is_a? Time
+
+                          value.to_i.to_s
+                        else
+                          value
+      end
+
+      { type => converted_value }
+    end
+
+    def _storage_type
+      (storage_type.presence || TezosClient.new.entrypoint(contract_address, entrypoint)).deep_symbolize_keys
+    end
+
+    def storage_type_or_contract_address_presence
+      return if storage_type.present? ^ (contract_address.present? && entrypoint.present?)
+
+      errors.add(:base,
+                 "You should provide the contract_address and the entrypoint only if storage_type is not provided")
+    end
+end
