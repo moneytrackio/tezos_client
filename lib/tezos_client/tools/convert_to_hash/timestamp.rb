13bc13a59ff045b407af64a6ceee121431eb284e
Pierre Michard <pierre@moneytrack.io>
2021-03-04 09:59:19 +0100

    Update lib/tezos_client/tools/convert_to_hash/timestamp.rb
    Co-authored-by: EL KESRI Othmane <elkesri.othmane@gmail.com>

--- lib/tezos_client/tools/convert_to_hash/timestamp.rb
+++ lib/tezos_client/tools/convert_to_hash/timestamp.rb
@@ -8,7 +8,7 @@ class TezosClient
           if data.key? :int
             Time.zone.at(data[:int].to_i)
           elsif data.key? :string
-            return Time.zone.at(data[:string].to_i) if data[:string].match?(/\A[\d]*\z/)
+            return Time.zone.at(data[:string].to_i) if data[:string].match?(/\A\d+\z/)
 
             Time.zone.parse(data[:string])
           else
